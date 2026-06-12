f0632c06c922a3f204bc863091eb463d2d4f01d6
arilin616-art <arilin616@gmail.com>
2026-06-12 19:49:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/dharma0610/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
